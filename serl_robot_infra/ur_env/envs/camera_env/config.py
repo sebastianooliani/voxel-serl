38403a62d1fadf07d0342449e36a89e99f288602
sebastianooliani <sebastiano.oliani@gmail.com>
2024-10-16 14:35:55 +0200

    added robot ips

--- serl_robot_infra/ur_env/envs/camera_env/config.py
+++ serl_robot_infra/ur_env/envs/camera_env/config.py
@@ -1,5 +1,6 @@
 from ur_env.envs.ur5_env import DefaultEnvConfig, DualRobotDefaultEnvConfig
 import numpy as np
+import math
 
 
 class UR5CameraConfig(DefaultEnvConfig):
@@ -11,7 +12,7 @@ class UR5CameraConfig(DefaultEnvConfig):
     ABS_POSE_LIMIT_LOW = np.array([-0.2, -0.7, - 0.006, 2.8, -0.18, -3.2])
     ACTION_SCALE = np.array([0.02, 0.1, 1.], dtype=np.float32)
 
-    ROBOT_IP: str = "172.22.22.2"
+    ROBOT_IP: str = "192.168.1.66"
     CONTROLLER_HZ = 100
     GRIPPER_TIMEOUT = 2000  # in milliseconds
     ERROR_DELTA: float = 0.05
@@ -41,7 +42,7 @@ class UR5CameraConfigBox5(DefaultEnvConfig):
     ABS_POSE_LIMIT_LOW = np.array([-0.49, -0.75, -0.006, 2.8, -0.18, -3.2])
     ACTION_SCALE = np.array([0.02, 0.1, 1.], dtype=np.float32)
 
-    ROBOT_IP: str = "172.22.22.2"
+    ROBOT_IP: str = "192.168.1.66"
     CONTROLLER_HZ = 100
     GRIPPER_TIMEOUT = 2000  # in milliseconds
     ERROR_DELTA: float = 0.05
@@ -69,6 +70,7 @@ class UR5CameraConfigFinal(DefaultEnvConfig):  # config for 10 boxes
         [0.4391, - 1.5926, 2.3356, - 2.3129, - 1.5668, - 1.1115],
         [0.1815, - 1.2945, 1.8964, - 2.1719, - 1.5658, - 1.3841],
     ])
+
     RANDOM_RESET = True
     RANDOM_XY_RANGE = (0.0,)
     RANDOM_ROT_RANGE = (0.04,)
@@ -77,11 +79,11 @@ class UR5CameraConfigFinal(DefaultEnvConfig):  # config for 10 boxes
     ABS_POSE_RANGE_LIMITS = np.array([0.36, 0.83])
     ACTION_SCALE = np.array([0.02, 0.1, 1.], dtype=np.float32)
 
-    ROBOT_IP: str = "172.22.22.2"
+    ROBOT_IP: str = "192.168.1.66" #"172.22.22.2" # "192.168.1.66"
     CONTROLLER_HZ = 100
     GRIPPER_TIMEOUT = 2000  # in milliseconds
     ERROR_DELTA: float = 0.05
-    FORCEMODE_DAMPING: float = 0.02  # faster but more vulnerable to crash...
+    FORCEMODE_DAMPING: float = 0.1  # faster but more vulnerable to crash...
     FORCEMODE_TASK_FRAME = np.zeros(6)
     FORCEMODE_SELECTION_VECTOR = np.ones(6, dtype=np.int8)
     FORCEMODE_LIMITS = np.array([0.5, 0.5, 0.5, 1., 1., 1.])
@@ -129,8 +131,8 @@ class UR5CameraConfigFinalEvaluation(UR5CameraConfigFinal):
 
 
 class UR5CameraConfigDualRobot(DualRobotDefaultEnvConfig):
-    RESET_Q = np.array([[1.3502, -1.2897, 1.9304, -2.2098, -1.5661, 1.4027,
-                         1.3502, -1.2897, 1.9304, -2.2098, -1.5661, 1.4027]])
+    RESET_Q = np.array([[0, -math.pi/2, math.pi/2, -math.pi/2, -math.pi/2, 0.,
+                         math.pi, -math.pi/2, math.pi/2, -math.pi/2, -math.pi/2, 0.]])
     RANDOM_RESET = False
     RANDOM_XY_RANGE = (0.00,)
     RANDOM_ROT_RANGE = (0.0,)
@@ -138,8 +140,8 @@ class UR5CameraConfigDualRobot(DualRobotDefaultEnvConfig):
     ABS_POSE_LIMIT_LOW = np.array([-0.2, -0.7, - 0.006, 2.8, -0.18, -3.2])
     ACTION_SCALE = np.array([0.02, 0.1, 1.], dtype=np.float32)
 
-    ROBOT_IP_1: str = "172.17.0.2"
-    ROBOT_IP_2: str = "172.17.0.3"
+    ROBOT_IP_1: str = "192.168.1.66" # docker "172.17.0.2"
+    ROBOT_IP_2: str = "192.168.1.33" # docker "172.17.0.3"
     ROBOT_PORT_1: int = 30004
     ROBOT_PORT_2: int = 40004
     CONTROLLER_HZ = 100
