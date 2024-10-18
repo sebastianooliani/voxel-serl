9726e8e8124cb94aa104e1ea74b4752b2b11498f
sebastianooliani <sebastiano.oliani@gmail.com>
2024-10-18 10:32:07 +0200

    first working behavioral cloning

--- examples/box_picking_bc/run_rbc_actor.sh
+++ examples/box_picking_bc/run_rbc_actor.sh
@@ -2,10 +2,10 @@ export XLA_PYTHON_CLIENT_PREALLOCATE=false && \
 export XLA_PYTHON_CLIENT_MEM_FRACTION=.3 && \
 which python && \
 python bc_policy.py "$@" \
-    --env box_picking_camera_env_dual_robot \
+    --env box_picking_camera_env \
     --exp_name=bc_drq_dual_policy \
     --seed 42 \
     --batch_size 256 \
-    --demo_paths "test.pkl" \
+    --demo_paths "ur5_test_20_demos_2024-10-17_15-41-46.pkl" \
     --eval_checkpoint_step 0 \
     --debug # wandb is disabled when debug
