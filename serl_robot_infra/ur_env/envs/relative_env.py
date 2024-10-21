a66182689e8dbb1e59e24f7913ea66574ffee470
sebastianooliani <sebastiano.oliani@gmail.com>
2024-10-21 16:31:21 +0200

    new class, dual relative env to transform obs in the ee frame

--- serl_robot_infra/ur_env/envs/relative_env.py
+++ serl_robot_infra/ur_env/envs/relative_env.py
@@ -113,3 +113,106 @@ class RelativeFrame(gym.Wrapper):
         action[:3] = self.rotation_matrix_reset.transpose() @ action[:3]
         action[3:6] = self.rotation_matrix_reset.transpose() @ action[3:6]
         return action
+
+
+class DualRelativeFrame(RelativeFrame):
+    """
+    This wrapper transforms the observation and action to be expressed in the end-effector frame.
+    Optionally, it can transform the tcp_pose into a relative frame defined as the reset pose.
+
+    This wrapper is expected to be used on top of the Dual UR5 environment, which has the following
+    observation space:
+    {
+        "state": spaces.Dict(
+            {
+                "tcp_pose": spaces.Box(-np.inf, np.inf, shape=(14,)), # xyz + quat
+                "tcp_vel": spaces.Box(-np.inf, np.inf, shape=(12,)),
+                "tcp_force": spaces.Box(-np.inf, np.inf, shape=(6,)),
+                "tcp_torque": spaces.Box(-np.inf, np.inf, shape=(6,)),
+                "gripper_state": spaces.Box(-np.inf, np.inf, shape=(4,)),
+            }
+        ),
+        ......
+    }, and at least 6 DoF action space with (x, y, z, rx, ry, rz, ...)
+    """
+    def __init__(self, env: Env):
+        super().__init__(env, include_relative_pose=True)
+
+    def reset(self, **kwargs):
+        obs, info = self.env.reset(**kwargs)
+
+        self.rotation_matrix_1 = construct_rotation_matrix(obs["state"]["tcp_pose"][:7])
+        self.rotation_matrix_2 = construct_rotation_matrix(obs["state"]["tcp_pose"][7:])
+        self.rotation_matrix_reset_1 = self.rotation_matrix_1.copy()
+        self.rotation_matrix_reset_2 = self.rotation_matrix_2.copy()
+
+        if self.include_relative_pose:
+            # Update transformation matrix from the reset pose's relative frame to base frame
+            self.T_r_o_inv_1 = np.linalg.inv(
+                construct_homogeneous_matrix(obs["state"]["tcp_pose"][:7])
+            )
+            self.T_r_o_inv_2 = np.linalg.inv(
+                construct_homogeneous_matrix(obs["state"]["tcp_pose"][7:])
+            )
+        
+        # Transform observation to spatial frame
+        return self.transform_observation(obs), info
+    
+    def transform_observation(self, obs):
+        """
+        Transform observations from spatial(base) frame into body(end-effector) frame
+        using the rotation and homogeneous matrix
+        """
+        obs["state"]["tcp_vel"][:3] = self.rotation_matrix_reset_1.transpose() @ obs["state"]["tcp_vel"][:3]
+        obs["state"]["tcp_vel"][3:6] = self.rotation_matrix_reset_1.transpose() @ obs["state"]["tcp_vel"][3:6]
+
+        obs["state"]["tcp_vel"][6:9] = self.rotation_matrix_reset_2.transpose() @ obs["state"]["tcp_vel"][6:9]
+        obs["state"]["tcp_vel"][9:12] = self.rotation_matrix_reset_2.transpose() @ obs["state"]["tcp_vel"][9:12]
+
+        obs["state"]["tcp_force"][:3] = self.rotation_matrix_1.transpose() @ obs["state"]["tcp_force"][:3]
+        obs["state"]["tcp_force"][3:6] = self.rotation_matrix_2.transpose() @ obs["state"]["tcp_force"][3:6]
+        obs["state"]["tcp_torque"][:3] = self.rotation_matrix_1.transpose() @ obs["state"]["tcp_torque"][:3]
+        obs["state"]["tcp_torque"][3:6] = self.rotation_matrix_2.transpose() @ obs["state"]["tcp_torque"][3:6]
+
+        if self.include_relative_pose:
+            T_b_o = construct_homogeneous_matrix(obs["state"]["tcp_pose"][:7])
+            T_b_r = self.T_r_o_inv @ T_b_o
+
+            # Reconstruct transformed tcp_pose vector
+            p_b_r = T_b_r[:3, 3]
+            theta_b_r = R.from_matrix(T_b_r[:3, :3]).as_quat()
+            obs["state"]["tcp_pose"][:7] = np.concatenate((p_b_r, theta_b_r))
+
+            T_b_o = construct_homogeneous_matrix(obs["state"]["tcp_pose"][7:])
+            T_b_r = self.T_r_o_inv @ T_b_o
+
+            # Reconstruct transformed tcp_pose vector
+            p_b_r = T_b_r[:3, 3]
+            theta_b_r = R.from_matrix(T_b_r[:3, :3]).as_quat()
+            obs["state"]["tcp_pose"][7:] = np.concatenate((p_b_r, theta_b_r))
+
+        return obs
+    
+    def transform_action(self, action: np.ndarray):
+        """
+        Transform action from body(end-effector) frame into spatial(base) frame
+        using the rotation matrix
+        """
+        action = np.array(action)  # in case action is a jax read-only array
+        action[:3] = self.rotation_matrix_reset_1 @ action[:3]
+        action[3:6] = self.rotation_matrix_reset_1 @ action[3:6]
+        action[6:9] = self.rotation_matrix_reset_2 @ action[6:9]
+        action[9:12] = self.rotation_matrix_reset_2 @ action[9:12]
+        return action
+
+    def transform_action_inv(self, action: np.ndarray):
+        """
+        Transform action from spatial(base) frame into body(end-effector) frame
+        using the rotation matrix.
+        """
+        action = np.array(action)
+        action[:3] = self.rotation_matrix_reset_1.transpose() @ action[:3]
+        action[3:6] = self.rotation_matrix_reset_1.transpose() @ action[3:6]
+        action[6:9] = self.rotation_matrix_reset_2.transpose() @ action[6:9]
+        action[9:12] = self.rotation_matrix_reset_2.transpose() @ action[9:12]
+        return action
