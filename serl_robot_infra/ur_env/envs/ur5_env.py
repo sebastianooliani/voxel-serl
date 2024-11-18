50f421eab5d42c0c208b00b7473dcdb46ca2cfc1
sebastianooliani <sebastiano.oliani@gmail.com>
2024-11-18 15:47:47 +0100

    adjusted naming convention

--- serl_robot_infra/ur_env/envs/ur5_env.py
+++ serl_robot_infra/ur_env/envs/ur5_env.py
@@ -790,21 +790,21 @@ class UR5DualRobotEnv(UR5Env):
         image_space_definition = {}
         if camera_mode in ["rgb", "grey", "both"]:
             channel = 1 if camera_mode == "grey" else 3
-            if "wrist_ROBOT_1" in config.REALSENSE_CAMERAS.keys():
+            if "wrist" in config.REALSENSE_CAMERAS.keys():
                 image_space_definition["wrist"] = gym.spaces.Box(
                     0, 255, shape=(128, 128, channel), dtype=np.uint8
                 )
-            if "wrist_ROBOT_2" in config.REALSENSE_CAMERAS.keys():
+            if "wrist_2" in config.REALSENSE_CAMERAS.keys():
                 image_space_definition["wrist_2"] = gym.spaces.Box(
                     0, 255, shape=(128, 128, channel), dtype=np.uint8
                 )
 
         if camera_mode in ["depth", "both"]:
-            if "wrist_ROBOT_1" in config.REALSENSE_CAMERAS.keys():
+            if "wrist" in config.REALSENSE_CAMERAS.keys():
                 image_space_definition["wrist_depth"] = gym.spaces.Box(
                     0, 255, shape=(128, 128, 1), dtype=np.uint8
                 )
-            if "wrist_ROBOT_2" in config.REALSENSE_CAMERAS.keys():
+            if "wrist_2" in config.REALSENSE_CAMERAS.keys():
                 image_space_definition["wrist_2_depth"] = gym.spaces.Box(
                     0, 255, shape=(128, 128, 1), dtype=np.uint8
                 )
