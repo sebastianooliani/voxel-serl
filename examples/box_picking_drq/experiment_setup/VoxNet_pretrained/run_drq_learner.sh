b97054657e3c581bd565d54f6eaa331e53abf0e3
sebastianooliani <sebastiano.oliani@gmail.com>
2025-01-20 17:50:07 +0100

    scripts for sac/drq + images

--- examples/box_picking_drq/experiment_setup/VoxNet_pretrained/run_drq_learner.sh
+++ examples/box_picking_drq/experiment_setup/VoxNet_pretrained/run_drq_learner.sh
@@ -1,20 +1,23 @@
 export XLA_PYTHON_CLIENT_PREALLOCATE=false && \
-export XLA_PYTHON_CLIENT_MEM_FRACTION=.1 && \
+export XLA_PYTHON_CLIENT_MEM_FRACTION=.3 && \
 python /home/sebastiano/voxel-serl/examples/box_picking_drq/drq_policy.py "$@" \
-    --actor \
+    --learner \
     --env box_picking_camera_env_dual_robot \
+    --wandb_project "dual_lift_voxnet_pretrained" \
     --exp_name="dual lift voxnet pretrained" \
     --camera_mode pointcloud \
     --max_traj_length 100 \
     --seed 1 \
-    --max_steps 20000 \
+    --max_steps 25000 \
     --random_steps 0 \
     --training_starts 500 \
     --utd_ratio 8 \
     --batch_size 128 \
-    --eval_period 0 \
+    --checkpoint_period 500 \
+    --checkpoint_path /home/sebastiano/voxel-serl/examples/box_picking_drq/experiment_setup/VoxNet_pretrained/checkpoints \
+    --demo_path /home/sebastiano/voxel-serl/examples/box_picking_drq/box_picking_20_demos_2025-01-20_17-09-45_dual_lift_pcd.pkl \
     --dual \
     --encoder_type voxnet-pretrained \
     --state_mask dual \
     --encoder_bottleneck_dim 128 \
-    --debug
+    # --debug
--- examples/box_picking_drq/experiment_setup/VoxNet_pretrained/run_sac_learner.sh
+++ examples/box_picking_drq/experiment_setup/VoxNet_pretrained/run_sac_learner.sh
@@ -1,9 +1,10 @@
 export XLA_PYTHON_CLIENT_PREALLOCATE=false && \
 export XLA_PYTHON_CLIENT_MEM_FRACTION=.3 && \
-python /home/sebastiano/voxel-serl/examples/box_picking_drq/drq_policy.py "$@" \
+python /home/sebastiano/voxel-serl/examples/box_picking_sac/sac_policy.py "$@" \
     --learner \
     --env box_picking_camera_env_dual_robot \
-    --exp_name="dual lift voxnet pretrained" \
+    --wandb_project "dual_lift_voxnet_pretrained" \
+    --exp_name="sac dual lift voxnet pretrained" \
     --camera_mode pointcloud \
     --max_traj_length 100 \
     --seed 1 \
@@ -12,11 +13,11 @@ python /home/sebastiano/voxel-serl/examples/box_picking_drq/drq_policy.py "$@" \
     --training_starts 500 \
     --utd_ratio 8 \
     --batch_size 128 \
-    --checkpoint_period 1000 \
+    --checkpoint_period 500 \
     --checkpoint_path /home/sebastiano/voxel-serl/examples/box_picking_drq/experiment_setup/VoxNet_pretrained/checkpoints \
-    --demo_path /home/sebastiano/voxel-serl/examples/box_picking_drq/box_picking_20_demos_2025-01-10_15-46-18_dual_lift_pcd.pkl \
+    --demo_path /home/sebastiano/voxel-serl/examples/box_picking_bc/ur5_test_20_demos_2025-01-20_16-16-36.pkl \
     --dual \
     --encoder_type voxnet-pretrained \
     --state_mask dual \
     --encoder_bottleneck_dim 128 \
-    --debug
+    # --debug
