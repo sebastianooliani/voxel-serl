0e9807e9f769c5e44c3096be093ba5bcfc4db616
sebastianooliani <sebastiano.oliani@gmail.com>
2025-02-14 13:56:08 +0100

    switched to moveL. Working except when vision fails

--- serl_robot_infra/robot_controllers/ur5_controller.py
+++ serl_robot_infra/robot_controllers/ur5_controller.py
@@ -435,23 +435,31 @@ class UrImpedanceController(threading.Thread):
         # position is in a rotated world frame
         box_position = self.box.get_box_position()
         box_orientation = self.box.get_box_orientation()
+        rot = R.from_rotvec(box_orientation).as_matrix()
         size = self.box.get_box_size()
-        box_position = self.config.WF_rot @ box_position
-
         actual_pose = np.array(self.ur_receive.getActualTCPPose())
-        box_position = np.concatenate([box_position, actual_pose[3:]])
+
+        traslation = np.array([0., size[1] / 2, 0.])
+        error = np.array([0.03, 0., 0.])
+        
         # move to box position
         if self.robot_ip[-2:] == "66":
-            box_position[1] += size[1] / 2 + 0.0
-            box_position[2] += size[2] / 2 + 0.17
-            success = self.ur_control.moveJ_IK(box_position, speed=1, acceleration=0.8)
+            box_position = box_position - rot @ traslation
+            box_position = self.config.WF_rot @ box_position + error
+            box_position = np.concatenate([box_position, actual_pose[3:]])
+            # box_position[1] += size[1] / 2 + 0.0
+            box_position[2] += size[2] / 2 + 0.25
+            success = self.ur_control.moveL(box_position, speed=0.25, acceleration=0.8)
         elif self.robot_ip[-2:] == "33":
-            box_position[1] += - size[1] / 2 + 0.2
-            box_position[2] += size[2] / 2 + 0.17
+            box_position = box_position + rot @ traslation
+            box_position = self.config.WF_rot @ box_position + error
+            box_position = np.concatenate([box_position, actual_pose[3:]])
             # go back to robot's frame
+            # box_position[1] += - size[1] / 2 + 0.0
             position = np.linalg.inv(self.T_O1_O2) @ np.concatenate([box_position[:3], [1.]])
+            position[2] += size[2] / 2 + 0.25
             box_position = np.concatenate([position[:3], box_position[3:]])
-            success = self.ur_control.moveJ_IK(box_position, speed=1, acceleration=0.8)
+            success = self.ur_control.moveL(box_position, speed=0.25, acceleration=0.8)
 
         await self._update_robot_state()
         with self.lock:
