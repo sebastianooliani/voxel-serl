923adf830bd510665b2a18e4d61e577c499dde8a
sebastianooliani <sebastiano.oliani@gmail.com>
2025-03-27 17:06:21 +0100

    removed forces and torques from inairrot

--- serl_robot_infra/ur_env/envs/camera_env/box_picking_camera_env.py
+++ serl_robot_infra/ur_env/envs/camera_env/box_picking_camera_env.py
@@ -595,8 +595,6 @@ class UR5CameraEnvDualRobotInAirRotation(UR5DualRobotEnv):
             "tcp_pose": self.curr_pos,
             "tcp_vel": self.curr_vel,
             "gripper_state": self.gripper_state,
-            "tcp_force": self.curr_force,
-            "tcp_torque": self.curr_torque,
             "action": action,
             # TODO: add my custom observations here
             "tcp_pos_diff": self.curr_pos[:3] - (self.T_O1_O2 @ np.concatenate([self.curr_pos[7:10], [1.]]))[:3],
